4c4065e1f3b702ff351e3f52d8657981383b2a40
SangNQ <ngsang998@gmail.com>
2026-04-30 17:04:12 +0800

    add

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,4 +1,9 @@
-from src.utils.lmdb import build_lmdb
 import pathlib
+from pathlib import Path
+import sys
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+from src.utils.lmdb import build_lmdb
 
 build_lmdb(pathlib.Path('data/VSPW'), pathlib.Path('data/VSPW/vspw.lmdb'), map_size = 1024 * 1024 * 1024 * 50)
