962c820aba914484a9ae92169bc4f96e200d3682
SangNQ <ngsang998@gmail.com>
2026-04-30 12:08:06 +0800

    add new

--- src/utils/lmdb.py
+++ src/utils/lmdb.py
@@ -1,39 +1,51 @@
 import lmdb
-import pickle
 from pathlib import Path
-from PIL import Image
-import io
 import tqdm
 
 def build_lmdb(root: Path, lmdb_path: Path, map_size):
-    env = lmdb.open(str(lmdb_path), map_size=map_size,readahead=False,
-    meminit=False,
-    lock=False,)
+    env = lmdb.open(
+        str(lmdb_path),
+        map_size=map_size,
+        readahead=False,
+        meminit=False,
+        lock=False,
+    )
 
-    with env.begin(write=True) as txn:
-        for video_dir in tqdm.tqdm((root / "data").iterdir()):
-            if not video_dir.is_dir():
+    txn = env.begin(write=True)
+    commit_interval = 2000  # 🔥 batch size
+
+    count = 0
+
+    for video_dir in tqdm.tqdm((root / "data").iterdir()):
+        if not video_dir.is_dir():
+            continue
+
+        vid = video_dir.name
+        img_dir = video_dir / "origin"
+        mask_dir = video_dir / "mask"
+
+        for mask_path in mask_dir.glob("*.png"):
+            frame = mask_path.stem
+            img_path = img_dir / f"{frame}.jpg"
+
+            if not img_path.exists():
                 continue
 
-            vid = video_dir.name
-            img_dir = video_dir / "origin"
-            mask_dir = video_dir / "mask"
+            with open(img_path, "rb") as f:
+                img_bytes = f.read()
 
-            for mask_path in mask_dir.glob("*.png"):
-                frame = mask_path.stem
-                img_path = img_dir / f"{frame}.jpg"
+            with open(mask_path, "rb") as f:
+                mask_bytes = f.read()
 
-                if not img_path.exists():
-                    continue
+            txn.put(f"{vid}/{frame}_img".encode(), img_bytes)
+            txn.put(f"{vid}/{frame}_mask".encode(), mask_bytes)
 
-                # image
-                with open(img_path, "rb") as f:
-                    img_bytes = f.read()
+            count += 1
 
-                with open(mask_path, "rb") as f:
-                    mask_bytes = f.read()
+            # 🔥 batch commit
+            if count % commit_interval == 0:
+                txn.commit()
+                txn = env.begin(write=True)
 
-                txn.put(f"{vid}/{frame}_img".encode(), img_bytes)
-                txn.put(f"{vid}/{frame}_mask".encode(), mask_bytes)
-
+    txn.commit()
     env.close()
